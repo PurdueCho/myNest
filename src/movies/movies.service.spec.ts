e6d82689274a9ed0f3555ca8fbdc37110a903be3
Younghyun Cho <cho242@purdue.edu>
2021-04-27 23:42:20 +0900

    unit test added

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -49,4 +49,62 @@ describe('MoviesService', () => {
       }
     });
   });
+
+  describe("deleteOne", ()=> {
+
+    it("deletes a movie", ()=>{
+      service.create( {
+        title:"Test Movie",
+        genres: ['test'],
+        year: 2550
+      });
+      const beforeDelete  = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it("should return a 404", ()=> {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe("create", () => {
+    it ("should create a movie", () => {
+      const beforeCreate = service.getAll().length;
+      service.create( {
+        title:"Test Movie",
+        genres: ['test'],
+        year: 2550
+      });
+      const afterCreate = service.getAll().length;
+      console.log(beforeCreate, afterCreate);
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe("update", ()=>{
+    it("should update a movie", ()=> {
+      service.create( {
+        title:"Test Movie",
+        genres: ['test'],
+        year: 2550
+      });
+      service.update(1, {title: 'Updated Test'});
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual("Updated Test");
+    });
+    it("should thorw a NotFoundExecption", ()=> {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
 });
